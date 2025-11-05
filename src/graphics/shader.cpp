f6fe65fca01dbc68f1075e0a061d4584e96081aa
dvuvud <david.buhrman.0648@student.uu.se>
2025-11-05 14:42:20 +0100

    Add set function 4d matrix uniforms in shader class

--- src/graphics/shader.cpp
+++ src/graphics/shader.cpp
@@ -2,6 +2,8 @@
 #include <glad/glad.h>
 #include <fstream>
 #include <sstream>
+#include <iostream>
+#include <glm/gtc/type_ptr.hpp>
 
 Shader::Shader(const char* vertexPath, const char* fragmentPath) {
 	// 1. retrieve the vertex/fragment source code from filePath
@@ -97,4 +99,8 @@ void Shader::setFloat(const std::string &name, float value) const {
 
 void Shader::setInt(const std::string &name, int value) const {
 	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setMat4(const std::string &name, const glm::mat4 &value) const {
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
 }
