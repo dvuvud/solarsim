ab32005d262d64bb67c2a40845ab5493ff79bc2c
dvuvud <carldavid.buhrman@gmail.com>
2025-11-05 14:42:20 +0100

    Add set function 4d matrix uniforms in shader class

--- src/graphics/shader.hpp
+++ src/graphics/shader.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <iostream>
+#include <string>
+#include <glm/glm.hpp>
 
 class Shader
 {
@@ -16,4 +17,5 @@ class Shader
 		void setBool(const std::string &name, bool value) const;  
 		void setInt(const std::string &name, int value) const;   
 		void setFloat(const std::string &name, float value) const;
+		void setMat4(const std::string &name, const glm::mat4& value) const;
 };
