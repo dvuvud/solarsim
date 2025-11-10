d25933b2379f407bd791f45afb4b027a18b12c06
dvuvud <carldavid.buhrman@gmail.com>
2025-11-10 01:19:09 +0100

    Add transform default costructor

--- src/simulation/entity.hpp
+++ src/simulation/entity.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <string>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -10,9 +9,9 @@
 #include "mesh.hpp"
 
 struct Transform {
-	glm::vec3 position;
-	glm::vec3 rotation;
-	glm::vec3 scale;
+	glm::vec3 position = glm::vec3(0.f,0.f,0.f);
+	glm::vec3 rotation = glm::vec3(0.f,0.f,0.f);
+	glm::vec3 scale = glm::vec3(1.f,1.f,1.f);
 
 	glm::mat4 getModelMatrix() const {
 		glm::mat4 model = glm::mat4(1.f);
@@ -27,21 +26,23 @@ struct Transform {
 
 class Entity {
 	public:
-		Entity(const std::string& name = "Entity") : name(name) {}
+		Entity(const Transform& transform, Mesh* mesh, Material* material)
+		: transform(transform), mesh(mesh), material(material) {}
 		virtual ~Entity() = default;
 
 		virtual void render(Shader& shader) = 0;
 
 		virtual void update(float deltaTime) = 0;
 
+		void setTransform(const Transform& transform) { this->transform = transform; }
 		Transform& getTransform() { return transform; }
 		const Transform& getTransform() const { return transform; }
 
-		void setMaterial(const std::shared_ptr<Material> material) { this->material = material; }
-		std::shared_ptr<Material> getMaterial() const { return material; }
+		void setMaterial(Material* material) { this->material = material; }
+		Material* getMaterial() const { return material; }
 
-		void setMesh(const std::shared_ptr<Mesh> mesh) { this->mesh = mesh; }
-		std::shared_ptr<Mesh> getMesh() const { return mesh; }
+		void setMesh(Mesh* mesh) { this->mesh = mesh; }
+		Mesh* getMesh() const { return mesh; }
 
 		void setPosition(const glm::vec3& position) { transform.position = position; }
 		glm::vec3 getPosition() const { return transform.position; }
@@ -51,11 +52,9 @@ class Entity {
 
 		void setScale(const glm::vec3& scale) { transform.scale = scale; }
 		glm::vec3 getScale() const { return transform.scale; }
-
 	protected:
 		Transform transform;
-		std::string name;
-		std::shared_ptr<Mesh> mesh;
-		std::shared_ptr<Material> material;
+		Mesh* mesh;
+		Material* material;
 };
 
