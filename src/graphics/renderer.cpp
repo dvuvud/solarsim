f8e5cd505c7c898414c6f129499633e9ac6a6baf
dvuvud <david.buhrman.0648@student.uu.se>
2025-11-10 14:13:03 +0100

    Add const ref to camera on renderer

--- src/graphics/renderer.cpp
+++ src/graphics/renderer.cpp
@@ -1,19 +1,21 @@
 #include "glad/glad.h"
 #include <graphics/renderer.hpp>
 
-Renderer::Renderer(std::vector<std::unique_ptr<Entity>>& entities) : entities(entities) {
+Renderer::Renderer(std::vector<std::unique_ptr<Entity>>& entities, const Camera* camera) : entities(entities), activeCamera(camera)
+{
 	glEnable(GL_DEPTH_TEST);
 	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 }
 
-void Renderer::render(const Camera &camera) {
+void Renderer::render()
+{
 	glClearColor(0.f, 0.f, 0.f, 0.f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	for (auto& entity : entities) {
 		Shader* shader = entity->getMaterial()->shader;
 		shader->bind();
-		shader->setMat4("view", camera.GetViewMatrix());
-		shader->setMat4("projection", camera.GetProjectionMatrix());
+		shader->setMat4("view", activeCamera->GetViewMatrix());
+		shader->setMat4("projection", activeCamera->GetProjectionMatrix());
 		entity->render(*shader);
 	}
 
