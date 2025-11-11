21bff6348162c8546522b07a880b3c7fb6d9f47a
dvuvud <carldavid.buhrman@gmail.com>
2025-11-11 23:38:04 +0100

    Add class for cube meshes

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,45 +1,8 @@
 #include <engine/engine.hpp>
+#include <mesh/cube.hpp>
 #include <iostream>
 
 namespace solarsim {
-	// Put common shapes in factory later
-	const std::vector<Vertex> vertices = {
-		// 8 unique corner vertices
-		{ {-0.5,-0.5,-0.5}, {0,0,0}, {0,0} }, // v0
-		{ { 0.5,-0.5,-0.5}, {0,0,0}, {1,0} }, // v1
-		{ { 0.5, 0.5,-0.5}, {0,0,0}, {1,1} }, // v2
-		{ {-0.5, 0.5,-0.5}, {0,0,0}, {0,1} }, // v3
-		{ {-0.5,-0.5, 0.5}, {0,0,0}, {0,0} }, // v4
-		{ { 0.5,-0.5, 0.5}, {0,0,0}, {1,0} }, // v5
-		{ { 0.5, 0.5, 0.5}, {0,0,0}, {1,1} }, // v6
-		{ {-0.5, 0.5, 0.5}, {0,0,0}, {0,1} }, // v7
-	};
-
-	const std::vector<unsigned int> indices = {
-		// Front face: uses v0, v1, v2, v3
-		0, 1, 2,   // first triangle
-		2, 3, 0,   // second triangle
-
-		// Back face (uses vertices 4,5,6,7)  
-		5, 4, 7,   // first triangle
-		7, 6, 5,   // second triangle
-
-		// Left face (uses vertices 4,0,3,7)
-		4, 0, 3,   // first triangle
-		3, 7, 4,   // second triangle
-
-		// Right face (uses vertices 1,5,6,2)
-		1, 5, 6,   // first triangle  
-		6, 2, 1,   // second triangle
-
-		// Bottom face (uses vertices 4,5,1,0)
-		4, 5, 1,   // first triangle
-		1, 0, 4,   // second triangle
-
-		// Top face (uses vertices 3,2,6,7)
-		3, 2, 6,   // first triangle
-		6, 7, 3,   // second triangle
-	};
 
 	Engine::Engine(unsigned int width, unsigned int height, const std::string& title)
 	{
@@ -56,7 +19,8 @@ namespace solarsim {
 		m_tmpMat = std::make_unique<Material>(m_tmpShader.get());
 		m_tmpLightShader = std::make_unique<Shader>("assets/shaders/shader.vs", "assets/shaders/light_source.fs");
 		m_tmpLightMat = std::make_unique<Material>(m_tmpLightShader.get());
-		m_tmpMesh = std::make_unique<Mesh>(vertices, indices);
+
+		m_tmpMesh = std::make_unique<Cube>();
 
 		m_simulation->spawnEntity(std::make_unique<Planet>(m_tmpMesh.get(), m_tmpMat.get(), glm::vec3(0.f,0.f,0.f)));
 		m_simulation->spawnEntity(std::make_unique<Planet>(m_tmpMesh.get(), m_tmpLightMat.get(), glm::vec3(3.f,3.f,-5.f)));
