aa839ad1b640c897b7a2a668bcb3687fb8ac950b
dvuvud <carldavid.buhrman@gmail.com>
2025-11-14 19:49:10 +0100

    Add dynamic directional light and propagate lighting updates to shaders

--- include/simulation/simulation.hpp
+++ include/simulation/simulation.hpp
@@ -7,26 +7,30 @@
 
 namespace solarsim {
 	class Entity;
+	class Sun;
 	class Simulation {
 		public:
 			Simulation();
 			~Simulation();
 
 			void spawnEntity(std::unique_ptr<Entity> p_entity);
+			void spawnSun(std::unique_ptr<Sun> p_sun);
 
 			void update(float deltaTime);        
 
 			void clearAllEntities();
 
-			const Grid& getGrid() const { return m_grid; }
+			const Grid* getGrid() const { return &m_grid; }
 			size_t getEntityCount() const { return m_entities.size(); };
 			// TODO: Make this function const and add API calls for the input manager to handle input
 			Camera* getCamera() { return &m_camera; }
 			const Camera* getCamera() const { return &m_camera; }
 			const std::vector<std::unique_ptr<Entity>>& getEntities() const { return m_entities; }
+			const Sun* getSun() const { return m_sun.get(); }
 		private:
 			Grid m_grid;
 			std::vector<std::unique_ptr<Entity>> m_entities;
+			std::unique_ptr<Sun> m_sun;
 			Camera m_camera;
 
 			// TODO: Implement physics system
