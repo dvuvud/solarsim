c59d74a2059289d39bf15eda525ea873a284f6dd
dvuvud <carldavid.buhrman@gmail.com>
2025-11-20 22:56:08 +0100

    Optimize model matrix getter using caching

--- include/components/transform_component.hpp
+++ include/components/transform_component.hpp
@@ -14,12 +14,23 @@ namespace solarsim {
 			glm::vec3 m_position{0.0f};
 			glm::quat m_rotation{1.0f, 0.0f, 0.0f, 0.0f};
 			glm::vec3 m_scale{1.0f};
-			glm::mat4 getModelMatrix() const {
-				glm::mat4 T = glm::translate(glm::mat4(1.0f), m_position);
-				glm::mat4 R = glm::toMat4(m_rotation);
-				glm::mat4 S = glm::scale(glm::mat4(1.0f), m_scale);
-				return T * R * S;
+
+			void setPosition(const glm::vec3& pos) { m_position = pos; m_dirty = true; }
+			void setRotation(const glm::quat& rot) { m_rotation = rot; m_dirty = true; }
+			void setScale(const glm::vec3& scale) { m_scale = scale; m_dirty = true; }
+
+			[[nodiscard]] glm::mat4 getModelMatrix() const noexcept {
+				if (m_dirty) {
+					m_cachedMatrix = glm::translate(glm::mat4(1.0f), m_position) *
+						glm::toMat4(m_rotation) *
+						glm::scale(glm::mat4(1.0f), m_scale);
+					m_dirty = false;
+				}
+				return m_cachedMatrix;
 			}
+		private:
+			mutable glm::mat4 m_cachedMatrix{1.0f};
+			mutable bool m_dirty{true};
 	};
 }
 
