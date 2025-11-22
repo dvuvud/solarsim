10bc1bf3a629ecae9377be34a60de94c970f1233
dvuvud <carldavid.buhrman@gmail.com>
2025-11-22 09:06:42 +0100

    Add shader loading to asset manager

--- include/managers/asset_manager.hpp
+++ include/managers/asset_manager.hpp
@@ -2,6 +2,10 @@
 
 #include <unordered_map>
 #include <string>
+#include <memory>
+#include <graphics/mesh.hpp>
+#include <graphics/material.hpp>
+#include <graphics/shader.hpp>
 
 namespace solarsim {
 	class AssetManager {
@@ -16,28 +20,38 @@ namespace solarsim {
 
 			std::shared_ptr<Mesh> LoadMesh(const std::string& meshID) {
 				auto it = loadedMeshes.find(meshID);
-				if (it != loadedMeshes.end()) {
-					return it->second;
-				}
+				if (it != loadedMeshes.end()) return it->second;
+				
 				// TODO: Implement assimp and replace the following with model loading
-				std::shared_ptr<Cube> mesh = std::make_shared<Cube>();
+				auto mesh = std::make_shared<Mesh>();
 				loadedMeshes[meshID] = mesh;
 				return mesh;
 			}
 			std::shared_ptr<Material> LoadMaterial(const std::string& materialID) {
 				auto it = loadedMaterials.find(materialID);
-				if (it != loadedMaterials.end()) {
-					return it->second;
-				}
+				if (it != loadedMaterials.end()) return it->second;
+
 				// TODO: Add functionality for loading materials
 				auto material = std::make_shared<Material>();
 				loadedMaterials[materialID] = material;
 				return material;
 			}
+			std::shared_ptr<Shader> LoadShader(const std::string& shaderID) {
+				auto it = loadedShaders.find(shaderID);
+				if (it != loadedShaders.end()) return it->second;
+
+				auto shader = std::make_shared<Shader>();
+				shader->programID = compileShader(shaderID);
+				loadedShaders[shaderID] = shader;
+				return shader;
+			}
 
 		private:
 			std::unordered_map<std::string, std::shared_ptr<Material>> loadedMaterials;
 			std::unordered_map<std::string, std::shared_ptr<Mesh>> loadedMeshes;
+			std::unordered_map<std::string, std::shared_ptr<Shader>> loadedShaders;
+
+			unsigned int compileShader(const std::string& shaderID);
 
 			AssetManager() {}
 			~AssetManager() {}
