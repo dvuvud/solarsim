169376a173685a0e8b2ea6c61cecd93eeac1096e
dvuvud <carldavid.buhrman@gmail.com>
2025-11-22 08:27:05 +0100

    Add function definitions to Registry

--- include/scene/registry.hpp
+++ include/scene/registry.hpp
@@ -2,12 +2,18 @@
 
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
+#include <tuple>
 #include "entity.hpp"
 
 namespace solarsim {
 	class Registry {
 		public:
-			Entity createEntity();
+			Entity createEntity() {
+				Entity e = nextEntity++;
+				entities.insert(e);
+				return e;
+			}
 
 			template<typename Component>
 				void addComponent(Entity e, Component c) {
@@ -16,19 +22,40 @@ namespace solarsim {
 
 			template<typename Component>
 				Component& getComponent(Entity e) {
-					return getComponentStorage<Component>()[e];
+					return getComponentStorage<Component>().at(e);
+				}
+
+			template<typename Component>
+				bool hasComponent(Entity e) {
+					auto& storage = getComponentStorage<Component>();
+					return storage.find(e) != storage.end();
 				}
 
 			template<typename... Components>
 				std::vector<Entity> view() {
-					
+					std::vector<Entity> result;
+					if constexpr (sizeof...(Components) == 0) return result;
+
+					auto& firstStorage = getComponentStorage<std::tuple_element_t<0, std::tuple<Components...>>>();
+
+					for (auto& [entity, comp] : firstStorage) {
+						bool hasAll = (hasComponent<Components>(entity) && ...);
+						if (hasAll) result.push_back(entity);
+					}
+
+					return result;
 				}
-					
 
 		private:
 			std::unordered_set<Entity> entities;
+			unsigned int nextEntity = 0;
 
+			// Each component type gets its own static storage
 			template<typename Component>
-				std::unordered_map<Entity, Component>& getComponentStorage();
+				std::unordered_map<Entity, Component>& getComponentStorage() {
+					static std::unordered_map<Entity, Component> storage;
+					return storage;
+				}
 	};
+
 }
