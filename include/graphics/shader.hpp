28d0d0dc2ca8eb8a82776c384e4b7bfb9546ff19
dvuvud <carldavid.buhrman@gmail.com>
2025-11-22 14:09:22 +0100

    Implement shader src compilation on Shader struct

--- include/graphics/shader.hpp
+++ include/graphics/shader.hpp
@@ -3,17 +3,64 @@
 #include <glad/glad.h>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/mat4x4.hpp>
+
+#include <iostream>
 #include <string>
 
 namespace solarsim {
 	struct Shader
 	{
 		unsigned int programID;
+
 		void use() {
 			glUseProgram(programID);
 		}
 		void setUniform(const std::string& name, const glm::mat4& value) {
 			glUniformMatrix4fv(glGetUniformLocation(programID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
 		}
+
+		void compile(const std::string& vertexSrc, const std::string& fragmentSrc) {
+			unsigned int vertex = glCreateShader(GL_VERTEX_SHADER);
+			const char* vSrc = vertexSrc.c_str();
+			glShaderSource(vertex, 1, &vSrc, nullptr);
+			glCompileShader(vertex);
+			checkCompileErrors(vertex, "VERTEX");
+
+			unsigned int fragment = glCreateShader(GL_FRAGMENT_SHADER);
+			const char* fSrc = fragmentSrc.c_str();
+			glShaderSource(fragment, 1, &fSrc, nullptr);
+			glCompileShader(fragment);
+			checkCompileErrors(fragment, "FRAGMENT");
+
+			unsigned int program = glCreateProgram();
+			glAttachShader(program, vertex);
+			glAttachShader(program, fragment);
+			glLinkProgram(program);
+			checkCompileErrors(program, "PROGRAM");
+
+			glDeleteShader(vertex);
+			glDeleteShader(fragment);
+
+			programID = program;
+		}
+
+		void checkCompileErrors(unsigned int shader, const std::string& type) {
+			int success;
+			char infoLog[1024];
+			if (type != "PROGRAM") {
+				glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+				if (!success) {
+					glGetShaderInfoLog(shader, 1024, nullptr, infoLog);
+					std::cerr << "Shader Compilation Error (" << type << "): " << infoLog << "\n";
+				}
+			} else {
+				glGetProgramiv(shader, GL_LINK_STATUS, &success);
+				if (!success) {
+					glGetProgramInfoLog(shader, 1024, nullptr, infoLog);
+					std::cerr << "Shader Linking Error (" << type << "): " << infoLog << "\n";
+				}
+			}
+		}
+
 	};
 }
